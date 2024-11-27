37c5628ea37386823240afb9962e4e28ad7ccd2b
Mohammad Reza Moradi <mohammadrezamoradi382@gmail.com>
2024-11-27 14:29:27 -0800

    f

--- functions/index.js
+++ functions/index.js
@@ -10,6 +10,12 @@
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
+// Example cloud function
+exports.helloWorld = onRequest((request, response) => {
+    logger.info("Hello logs!", { structuredData: true });
+    response.send("Hello from Firebase!");
+  });
+
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
