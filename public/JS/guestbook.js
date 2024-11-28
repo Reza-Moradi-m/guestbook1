ae194c05d17cd0478c3766c3559ca7a1f8c63abd
Mohammad Reza Moradi <mohammadrezamoradi382@gmail.com>
2024-11-27 23:15:54 -0800

    g

--- public/JS/guestbook.js
+++ public/JS/guestbook.js
@@ -1,5 +1,5 @@
 // Import Firebase services
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+import  initializeApp  from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
 import { getFirestore, collection, addDoc, query, orderBy, getDocs } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-storage.js";
 
