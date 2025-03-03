84a17cf4d20f63b31060b64ac4dfdcff0e69667c
Mohammad Reza Moradi <mohammadrezamoradi382@gmail.com>
2025-03-02 23:33:13 -0700

    u

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,23 +2,24 @@ const CACHE_NAME = "stellarious-cache-v1";
 const urlsToCache = [
   "/",
   "/index.html",
-  "/CSS/chatroom.css",
+  "/CSS/styles.css",
   "/CSS/responsive.css",
-  "/JS/chatroom.js",
-  "/images/logo-192x192.png",
-  "/images/logo-512x512.png"
+  "/JS/app.js",
+  "/JS/common.js",
+  "/images/logo.png"
 ];
 
-// Install Service Worker and cache static assets
+// Install Service Worker and Cache Assets
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
       return cache.addAll(urlsToCache);
     })
   );
 });
 
-// Fetch assets from cache when offline
+// Fetch Requests and Serve from Cache When Offline
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
